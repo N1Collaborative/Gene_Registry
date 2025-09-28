645e3e7a1a8038f50116a7febc820b6646658c7e
Marlen Lauffer <93141861+marlencharlotte@users.noreply.github.com>
2025-09-28 10:36:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-@@ -9,7 +9,7 @@
-# Mapping table names to file pathsign
+# Mapping table names to file paths
 TABLE_FILES = {
     "N1C_projects": "N1C_projects.xlsx",
     "marketed_drugs": "Marketed_drugs.xlsx",
-    "assessed_variants": "assessed_variants.xlsx"
+    "assessed_variants": "assessed_variants.xlsx",
 }
 
 
